4730dc4a78ca1b93fe9025ddd243e517445c587b
Unknown <mikaw2@hotmail.com>
2020-11-03 22:54:15 +0200

    make code draw mandelbrot

--- MandelBrot/mandelbrot.cpp
+++ MandelBrot/mandelbrot.cpp
@@ -28,11 +28,11 @@ void Mandelbrot::Init()
 	unsigned int slots[3] = { 0 , 1, 0 };
 
 	AddShader("../res/shaders/pickingShader"); //0 - click on 3d obj always in 0 pos.. even in 2d
-	AddShader("../res/shaders/mandelbrotShader"); // what we will write
-	//AddShader("../res/shaders/basicShader"); // 1
+	//AddShader("../res/shaders/mandelbrotShader"); // what we will write
+	AddShader("../res/shaders/basicShader"); // 1
 
-	AddTexture("../res/textures/pal.png", 1); // try returning to 1 later when running or change back to 0
-	//TextureDesine(800, 800); // replace shader
+	//AddTexture("../res/textures/pal.png", 1); // try returning to 1 later when running or change back to 0
+	TextureDesine(840, 840); // replace shader
 
 	AddMaterial(texIDs, slots, 1);
 
@@ -67,9 +67,9 @@ void Mandelbrot::Update(const glm::mat4 &MVP,const glm::mat4 &Model,const int sh
 	if(shaderIndx!=1)
 		s->SetUniform1i("sampler2", materials[shapes[pickedShape]->GetMaterial()]->GetSlot(1));
 	//send param to shader
-	s->SetUniform1ui("counter", counter); // timer
-	s->SetUniform1f("x", x); // x param
-	s->SetUniform1f("y", y); // y param - warning means we dont use it!
+	//s->SetUniform1ui("counter", counter); // timer
+	//s->SetUniform1f("x", x); // x param
+	//s->SetUniform1f("y", y); // y param - warning means we dont use it!
 	s->Unbind();
 }
 
@@ -100,17 +100,44 @@ void Mandelbrot::Motion()
 	}
 }
 
+double mapRange(int num, int total, double minR, double maxR) {
+	return num * ((maxR - minR) / total) + minR;
+}
+
 unsigned int Mandelbrot::TextureDesine(int width, int height)
 {
+	float p = 11;
+	float iter = 50;
+	float scale = 1.5;
 	unsigned char* data = new unsigned char[width * height * 4];
 	for (size_t i = 0; i < width; i++)
 	{
+		//float zy = 2 * y / height + ya;
 		for (size_t j = 0; j < height; j++)
 		{
-			data[(i * height + j) * 4] = (i + j) % 256;
-			data[(i * height + j) * 4 + 1] = 0;//(i + j * 2) % 256;
-			data[(i * height + j) * 4 + 2] = (i * 2 + j) % 256;
-			data[(i * height + j) * 4 + 3] = (i * 3 + j) % 256;
+			double cx = mapRange(i, width, -2, 1);
+			double cy = mapRange(j, height, -1.5, 1.5);
+			
+			int counter = 0;
+			double zx = 0;
+			double zy = 0;
+			while (counter < iter && zx*zx + zy*zy < 4.0) {
+				double c = zx * zx + zy * zy;
+				double t = pow(c, p/2)*cos(p*atan2(zy,zx)) + cx;
+				zy = pow(c, p / 2) * sin(p * atan2(zy, zx)) + cy;
+				zx = t;
+				counter++;
+			}
+
+			double color = mapRange(counter, iter, 0, 255);
+			if (counter == iter) {
+				color = 0;
+			}
+
+			data[(i * height + j) * 4] = 0; //color;//(i + j) % 256;
+			data[(i * height + j) * 4 + 1] = color;//color;//(i + j * 2) % 256;
+			data[(i * height + j) * 4 + 2] = color;//color;
+			data[(i * height + j) * 4 + 3] = color;
 		}
 	}
 	textures.push_back(new Texture(width, height));
